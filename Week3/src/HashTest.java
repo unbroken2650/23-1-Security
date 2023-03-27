e6aea34854c4773224c86f41f8e295bb850f7e0a
UnbroKen <unbroken2650@gmail.com>
2023-03-27 12:23:19 +0900

    week3

---
+++ Week3/src/HashTest.java
@@ -0,0 +1,33 @@
+
+import java.security.MessageDigest;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class HashTest {
+
+	public static void main(String args[]) throws Exception
+	{
+	    Security.addProvider(new BouncyCastleProvider());
+	    
+	    MessageDigest hash = MessageDigest.getInstance("SHA256", "BC");
+	
+		String student_no="12345678";
+		String student_department="software";
+		String student_name="hongildong";
+		
+
+//		hash.update(Utils.toByteArray(student_no));
+//		hash.update(Utils.toByteArray(student_department));
+//		hash.update(Utils.toByteArray(student_name));
+		
+		
+		byte[] out = hash.digest();
+	
+		
+		
+		System.out.println("Hash: "+ Utils.toHexString(out));
+		System.out.println(out);
+	
+	}
+}
