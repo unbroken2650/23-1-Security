960a532266acbdb4c6bee088e8efdff7d1a89e91
unbroken2650 <unbroken2650@gmail.com>
2023-03-27 22:59:20 +0900

    Week3

---
+++ Week3/src/FileHash.java
@@ -0,0 +1,39 @@
+
+import java.io.FileInputStream;
+import java.security.Security;
+import java.security.MessageDigest;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+public class FileHash {
+	
+	public static void main(String args[]) throws Exception
+	{
+		
+		Security.addProvider(new BouncyCastleProvider());
+
+		
+		int BUF_SIZE = 2048;
+		
+		byte[] buffer = new byte[BUF_SIZE];
+		MessageDigest digest = MessageDigest.getInstance("SHA256");
+		
+		
+		FileInputStream fis = new FileInputStream("C:\\Users\\hansu\\Downloads\\03. 암호학적 해쉬 함수.pdf");
+		
+		int read = BUF_SIZE;
+
+		while ((read = fis.read(buffer, 0, BUF_SIZE)) > 0) {
+
+			digest.update(buffer, 0, read);
+		}
+		
+		
+		byte[] out = digest.digest();
+
+		fis.close();
+		System.out.println("md = " + Utils.toHexString(out));
+
+	}
+
+}
